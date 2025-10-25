fe79a25354df244b68b797e4e98f4e50ed77edbb
Enes Yürekli <muhammedenesyurekli@gmail.com>
2025-10-25 16:50:17 +0300

    balta ve fırlatma eklendi

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -6,69 +6,80 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     private Rigidbody2D rb;
+
     private bool facingRight = true;
 
     public Animator animator;
 
-    // Combo değişkenleri
-    private int comboStep = 0; // 0 = idle, 1 = attack1, 2 = attack2
-    private float comboResetTime = 1f; // iki saldırı arasında izin verilen max süre
-    private float comboTimer = 0f;
+    public float nextAttackTime = 0.7f;
 
-    void Awake()
+    public int axeCount = 5;
+    public GameObject axePrefab;
+    public GameObject axeSpawnPoint;
+    public float nextThrowAxe = 2f;
+
+
+
+    private void Awake()
     {
         animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         Movement();
+
         Attack();
 
-        // Combo sıfırlama
-        if (comboStep > 0)
-        {
-            comboTimer -= Time.deltaTime;
-            if (comboTimer <= 0)
-            {
-                comboStep = 0;
-            }
-        }
-    }
+        ThrowAxe();
 
+    }
     private void Movement()
     {
+        // Hareket girdisini al
         float moveInputX = Input.GetAxisRaw("Horizontal");
         float moveInputY = Input.GetAxisRaw("Vertical");
 
-        if (!facingRight && moveInputX > 0)
+        // Karakterin y�n�n� �evir
+        if (facingRight == false && moveInputX > 0)
             Flip();
-        else if (facingRight && moveInputX < 0)
+        else if (facingRight == true && moveInputX < 0)
             Flip();
-
-        animator.SetFloat("Run", (moveInputX != 0 || moveInputY != 0) ? 1 : 0);
-
+        if (moveInputX != 0 || moveInputY != 0)
+            animator.SetFloat("Run", 1);
+        else
+            animator.SetFloat("Run", 0);
         rb.linearVelocity = new Vector3(moveInputX * moveSpeed, moveInputY * moveSpeed, 0);
     }
 
     private void Attack()
     {
-        if (Input.GetMouseButtonDown(0))
+        nextAttackTime -= Time.deltaTime;
+        if (Input.GetMouseButtonDown(0) && nextAttackTime <= 0)
         {
-            if (comboStep == 0)
-            {
-                animator.SetTrigger("Attack");
-                comboStep = 1;
-                comboTimer = comboResetTime;
-            }
-            else if (comboStep == 1)
-            {
-                animator.SetTrigger("Attack2");
-                comboStep = 0; // combo bitti
-            }
+            animator.SetTrigger("Attack");
+            nextAttackTime = 0.7f;
         }
     }
+
+    private void ThrowAxe()
+    {
+        nextThrowAxe -= Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(1) && axeCount > 0)
+        {
+            GameObject throwedAxe = Instantiate(axePrefab, axeSpawnPoint.transform.position, Quaternion.identity);
+            Rigidbody2D axeRb = throwedAxe.GetComponent<Rigidbody2D>();
+            axeRb.AddForce(new Vector2(facingRight ? 1 : -1, 0) * 500f);
+            axeCount--;
+            nextThrowAxe = 2f;
+        }
+    }
+
 
     void Flip()
     {
