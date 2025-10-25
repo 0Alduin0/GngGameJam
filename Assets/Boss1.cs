aa109b6d7c38ceb867c1b9003418e47c8f029c99
Enes Yürekli <muhammedenesyurekli@gmail.com>
2025-10-25 19:16:28 +0300

    yeni karakter assetleri eklendi

---
+++ Assets/Boss1.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boss1 : MonoBehaviour
+{
+    public GameObject bossPromptUI;  // Canvas içindeki panel
+    private bool playerInRange = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+            bossPromptUI.SetActive(true);
+            Time.timeScale = 0f; // Oyunu dondur
+        }
+    }
+
+    public void OnYesPressed()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("BossArena");
+    }
+
+    public void OnNoPressed()
+    {
+        bossPromptUI.SetActive(false);
+        Time.timeScale = 1f;
+        playerInRange = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            bossPromptUI.SetActive(false);
+            playerInRange = false;
+        }
+    }
+}
